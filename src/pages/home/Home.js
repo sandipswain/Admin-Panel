7abd21e6bf2c7bc6775cbe4bd8fa5306731b2e63
Sandip Swain <swainsandip9@gmail.com>
2022-07-24 00:28:02 +0530

    Made the widgets component

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,13 +2,19 @@ import React from "react";
 import "./home.scss";
 import Sidebar from "./../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
+import Widget from "../../components/widget/Widget";
 const Home = () => {
   return (
     <div className="home">
       <Sidebar />
       <div className="homeContainer">
         <Navbar />
-        container
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earnings" />
+          <Widget type="balance" />
+        </div>
       </div>
     </div>
   );
